48c9f1d6bca3615090bb6d276c71efef7bdd21a0
Chance Smith <chance@sodiumhalogen.com>
2016-05-14 13:36:04 -0500

    adds ability to create Groups

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,37 @@
+class GroupsController < ApplicationController
+  def index
+    @groups = Group.order(:id)
+  end
+
+  # GET /groups/1
+  # GET /groups/1.json
+  def show
+  end
+
+  # GET /groups/new
+  def new
+    @group = Group.new
+  end
+
+  # POST /groups
+  # POST /groups.json
+  def create
+    @group = Group.new(group_params)
+
+    respond_to do |format|
+      if @group.save
+        format.html { redirect_to groups_path, notice: 'Group was successfully created.' }
+        format.json { render :show, status: :created, location: @group }
+      else
+        format.html { render :new }
+        format.json { render json: @group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def group_params
+      params.require(:group).permit(:title)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :activities
   resources :tracks
   resources :activities
+  resources :groups
   root 'tracks#index'
 end
 
